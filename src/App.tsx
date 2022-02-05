6933f1a07d2c49c040895b2a774052c530792ca3
jhlov <jhlovv@gmail.com>
2022-02-05 13:10:48 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React from "react";
+import { HashRouter, Redirect, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
@@ -7,9 +8,15 @@ function App() {
 
   return (
     <div className="App">
-      <div
-        className={classNames(isMobile ? "mobile-container" : "container")}
-      ></div>
+      <div className={classNames(isMobile ? "mobile-container" : "container")}>
+        {/* todo */}
+        <HashRouter>
+          <Route path="/" exact>
+            <Redirect to="/app" />
+          </Route>
+          {/* <Route path="/app" component={app} /> */}
+        </HashRouter>
+      </div>
     </div>
   );
 }
